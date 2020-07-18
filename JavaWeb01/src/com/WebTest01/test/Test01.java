17e25bd68cfbb9ab51209871deb9a287eec274bd
faded828x <faded828x@gmail.com>
2020-07-18 21:11:45 +0800

    test

---
+++ JavaWeb01/src/com/WebTest01/test/Test01.java
@@ -0,0 +1,15 @@
+package com.WebTest01.test;
+
+import com.WebTest01.domain.Pet;
+import com.WebTest01.service.Impl.PetServiceImpl;
+import com.WebTest01.service.PetService;
+import com.WebTest01.util.ServiceFactory;
+
+public class Test01 {
+    public static void main(String[] args) {
+        PetService petService = (PetService) ServiceFactory.getService(new PetServiceImpl());
+        Pet pet = new Pet("ttt", "S", 9999);
+        petService.deletePet(pet);
+        int test;
+    }
+}
