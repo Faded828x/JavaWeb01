8f0eb548b3958cc49f95c39c0380b903040721c2
faded828x <faded828x@gmail.com>
2020-07-18 21:24:05 +0800

    test01

--- JavaWeb01/src/com/WebTest01/test/Test01.java
+++ JavaWeb01/src/com/WebTest01/test/Test01.java
@@ -10,5 +10,6 @@ public class Test01 {
         PetService petService = (PetService) ServiceFactory.getService(new PetServiceImpl());
         Pet pet = new Pet("ttt", "S", 9999);
         petService.deletePet(pet);
+        int test;
     }
 }
